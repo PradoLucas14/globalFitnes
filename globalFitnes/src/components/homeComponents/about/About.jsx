ddfd1abba82c953c63f3b90da14010a27ba68333
Prado Lucas Nahuel <lukasnahuelprado32@gmail.com>
2024-12-28 20:46:08 -0300

    se modifico el estilo de los textos y el boton ver más del componente about

--- globalFitnes/src/components/homeComponents/about/About.jsx
+++ globalFitnes/src/components/homeComponents/about/About.jsx
@@ -8,9 +8,21 @@ function About() {
       <div className="about-content">
         <img src={HeroImage} alt="Imagen sobre el gimnasio" className="about-image" />
         <div className="about-text">
-          <h2>Conoce más sobre nosotros</h2>
-          <p>En nuestro gimnasio, cada miembro es parte de nuestra familia. Trabajamos juntos para lograr tus objetivos de fitness.</p>
-        </div>
+            <h2>Conoce más sobre nosotros</h2>
+            <p>En nuestro gimnasio, cada miembro es parte de nuestra familia. Nos dedicamos a crear un ambiente inclusivo y motivador donde todos, 
+            sin importar su nivel de experiencia, puedan alcanzar sus metas de salud y bienestar. Nuestro equipo de entrenadores 
+            altamente calificados está aquí para apoyarte en cada paso de tu viaje fitness, proporcionándote entrenamientos personalizados y consejos prácticos. 
+            Ya sea que busques mejorar tu fuerza, resistencia, flexibilidad o simplemente mantenerte activo, trabajamos juntos para lograr tus objetivos. 
+            Nos enfocamos en ofrecer un espacio donde el esfuerzo y la disciplina se convierten en una experiencia gratificante, ¡y donde cada pequeño logro cuenta! 
+            Únete a nosotros y sé parte de un equipo que no solo se preocupa por tu rendimiento, sino también por tu bienestar integral.</p>
+            <h3>Horario de atención: Lunes a Sabado de 7:00 hrs a 21:00 hrs</h3>
+            <h3>Ubicacion: Balcarce n°1050 - Alderetes</h3>
+            <div className='btnAboutContainer'>
+            <button className='btnAbout'>
+                <span>ver más</span>
+            </button>
+          </div>
+          </div>
       </div>
     </div>
   );
